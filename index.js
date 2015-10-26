5e0f4cb6c9b4101512c99958d0c9551a202f5398
Eric Bower <neurosnap@gmail.com>
2015-10-26 13:38:35 -0400

    react stuff

--- index.js
+++ index.js
@@ -3,4 +3,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log('hi mom');
+window.addEventListener('DOMContentLoaded', function() {
+  renderDemo();
+
+  let ta = document.getElementById('input');
+  ta.addEventListener('input', debounce(function() {
+    renderDemo(this.value);
+  }));
+});
+
+class SentDemo extends React.Component {
+  constructor(props) { super(props); };
+  render() {
+    return (
+      <div>
+        <textarea id="input"></textarea>
+        <Sentence />
+      </div>
+    );
+  };
+}
+
+class Sentence extends React.Component {
+  constructor(props) { super(props); }
+  render() {
+    return (<div>A sentence!</div>);
+  }
+}
+
+function renderDemo(text='') {
+  ReactDOM.render(
+    <SentDemo text={ text } />,
+    document.getElementById('demo')
+  );
+}
+
+function debounce(func, delay=500) {
+  var timer;
+  return function() {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func.bind(this), delay);
+  }
+}
+
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    console.log(`Grabbing: ${url}`);
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+      if (ajax.readyState != XMLHttpRequest.DONE) return;
+      if (ajax.status != 200) {
+        reject(ajax);
+        return;
+      }
+      resolve(ajax);
+    };
+
+    ajax.open('GET', url, true);
+    ajax.send();
+  });
+}
+
--- public/index.js
+++ public/index.js
@@ -94,7 +94,15 @@ process.umask = function() { return 0; };
 },{}],2:[function(require,module,exports){
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x3, _x4, _x5) { var _again = true; _function: while (_again) { var object = _x3, property = _x4, receiver = _x5; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x3 = parent; _x4 = property; _x5 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -104,7 +112,95 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-console.log('hi mom');
+window.addEventListener('DOMContentLoaded', function () {
+  renderDemo();
+
+  var ta = document.getElementById('input');
+  ta.addEventListener('input', debounce(function () {
+    renderDemo(this.value);
+  }));
+});
+
+var SentDemo = (function (_React$Component) {
+  _inherits(SentDemo, _React$Component);
+
+  function SentDemo(props) {
+    _classCallCheck(this, SentDemo);
+
+    _get(Object.getPrototypeOf(SentDemo.prototype), 'constructor', this).call(this, props);
+  }
+
+  _createClass(SentDemo, [{
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        null,
+        _react2['default'].createElement('textarea', { id: 'input' }),
+        _react2['default'].createElement(Sentence, null)
+      );
+    }
+  }]);
+
+  return SentDemo;
+})(_react2['default'].Component);
+
+var Sentence = (function (_React$Component2) {
+  _inherits(Sentence, _React$Component2);
+
+  function Sentence(props) {
+    _classCallCheck(this, Sentence);
+
+    _get(Object.getPrototypeOf(Sentence.prototype), 'constructor', this).call(this, props);
+  }
+
+  _createClass(Sentence, [{
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        null,
+        'A sentence!'
+      );
+    }
+  }]);
+
+  return Sentence;
+})(_react2['default'].Component);
+
+function renderDemo() {
+  var text = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+  _reactDom2['default'].render(_react2['default'].createElement(SentDemo, { text: text }), document.getElementById('demo'));
+}
+
+function debounce(func) {
+  var delay = arguments.length <= 1 || arguments[1] === undefined ? 500 : arguments[1];
+
+  var timer;
+  return function () {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func.bind(this), delay);
+  };
+}
+
+function get(url) {
+  return new Promise(function (resolve, reject) {
+    console.log('Grabbing: ' + url);
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+      if (ajax.readyState != XMLHttpRequest.DONE) return;
+      if (ajax.status != 200) {
+        reject(ajax);
+        return;
+      }
+      resolve(ajax);
+    };
+
+    ajax.open('GET', url, true);
+    ajax.send();
+  });
+}
 
 },{"react":158,"react-dom":3}],3:[function(require,module,exports){
 'use strict';
