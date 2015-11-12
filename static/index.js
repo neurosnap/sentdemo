6c6b742b0363709a9ecde039f9fad4cfdb7a67d0
Eric Bower <neurosnap@gmail.com>
2015-11-11 22:58:43 -0500

    removed output file

--- static/index.js
+++ static/index.js
@@ -112,7 +112,7 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var placeholder = 'Hey there!  How\'s it going?\n\nMy initials are E.R.B. which is alright.\nHere\'s the U.S. at the beginning of the sentence and now I\'ll end the sentence with U.S.  This is great,\nI\'ll just call Sen. Bernie Sanders to help me get out of here.\n\nOr maybe a Rep. Lindsey Graham would be better...';
+var placeholder = 'Hey there!  How\'s it going?\n\nMy initials are E.R.B. which is alright.\nHere\'s the U.S. at the beginning of the sentence and now I\'ll end the sentence with U.S.  This is great,\nI\'ll just call Sen. Bernie Sanders to help me get out of here.\n\nOr maybe Rep. Lindsey Graham would be better...';
 
 var maxChars = 500;
 
