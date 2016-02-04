6696bcd1f6155bfc73de5c437610786d765d4beb
Eric Bower <neurosnap@gmail.com>
2016-02-03 23:20:09 -0500

    getting there!

--- index.js
+++ index.js
@@ -2,14 +2,75 @@
 
 import Rx from 'rx';
 import Cycle from '@cycle/core';
-import CycleDOM from '@cycle/dom';
+import { makeDOMDriver, div, textarea } from '@cycle/dom';
+import { makeHTTPDriver } from '@cycle/http';
 
-function main() {
+const placeholder = `I am a sentence!  How's it going?
 
+The english language has loads of periods that have nothing to do with sentence punctuation.
+
+Besides abbreviations like M.D. and initials containing periods, they can also end a sentence.
+
+Sentence tokenization is difficult when an abbreviation ends with a sentence,
+which is very common in news articles in the U.S.  There are custom initials like E.R.B. which
+also happens to be the initals for my name.`;
+
+const maxChars = 500;
+
+function main(sources) {
+  const getSentences$ = sources.DOM.select('#input').events('input')
+    .debounce(1000)
+    .map(e => e.target.value)
+    .startWith(placeholder)
+    .map(text => {
+      return {
+        method: 'GET',
+        url: '/sentences/?text=' + text,
+        text
+      };
+    });
+
+  const input$ = sources.HTTP
+    .mergeAll()
+    .map(res => {
+      console.log(res);
+      const positions = res.body.sentences;
+      const text = res.request.text;
+
+      let sentences = [];
+      let lastPos = 0;
+      for (let i = 0; i < positions.length; i++) {
+        let sentPos = positions[i];
+        let sentence = text.slice(lastPos, sentPos);
+        lastPos = sentPos;
+        sentence = sentence.trim();
+
+        if (!sentence) continue;
+
+        sentences.push(div('.sentence .border', sentence));
+      }
+
+      return { text, sentences };
+    });
+
+  const vtree$ = input$.map(data =>
+    div('.row', [
+      div('.col-left', [
+        textarea('#input', data.text)
+      ]),
+      div('.col-right', data.sentences)
+    ])
+  );
+
+  return {
+    DOM: vtree$,
+    HTTP: getSentences$
+  };
 }
 
 const drivers = {
-  DOM: CycleDOM.makeDOMDriver('#demo')
+  DOM: makeDOMDriver('#demo'),
+  HTTP: makeHTTPDriver()
 };
 
 Cycle.run(main, drivers);
